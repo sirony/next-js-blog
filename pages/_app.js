f2ba509bb12506a1e110401e5e15971ec9407bb8
MD SHARIFUL ISLAM <itteam.5@aieus.com>
2022-11-16 18:28:24 +0600

    first version of portfolio site

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import {  Fragment, useState } from 'react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function PL({ Component, pageProps }) {
+
+const [loading, setLoading] = useState(true);
+
+setTimeout(()=>{
+  setLoading(false);
+}, 5000)
+
+
+
+
+  return (
+    <>
+      <Component {...pageProps} /> 
+    </>
+    
+  )
 }
 
-export default MyApp
+export default PL
