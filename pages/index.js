a055f0c70912588c4494d6bc7320684df50552c1
MD SHARIFUL ISLAM <itteam.5@aieus.com>
2022-11-16 18:45:31 +0600

    build version

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,22 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
-import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export async function getStaticProps(){
-  const allPostsData = getSortedPostsData();
-  return{
-    props : {
-      allPostsData
-    }
-  }
-}
 
-export default function Home({ allPostsData }) {
+export default function Home() {
   const home = true;
+
+  const content = "Hello, I&quot;m <span className={`${utilStyles.boldText} ${utilStyles.redColor}`}>Shariful</span>. I&quot;m Software enginer and <span className={utilStyles.brownColor}>Full Stack Web Developer</span> from Bangladesh";
+
+
   return (
     <Layout home={home}>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <span className={`${utilStyles.boldText} ${utilStyles.redColor}`}>Shariful</span>. I'm Software enginer and <span className={utilStyles.brownColor}>Full Stack Web Developer</span> from Bangladesh</p>
+        <p>Hello, I am <span className={`${utilStyles.boldText} ${utilStyles.redColor}`}>Shariful</span>. I am Software enginer and <span className={utilStyles.brownColor}>Full Stack Web Developer</span> from Bangladesh</p>
         <p>
           Contact Information:<br></br>    
           Email: <a href="ailto:sirony25@gmail.com">sirony25@gmail.com</a>
